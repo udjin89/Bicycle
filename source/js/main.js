885c4ceea7939e3cac66a0a55cd27d30f2b1b0e1
Евгений <jenek95@mail.ru>
2021-05-21 06:26:27 +0300

    header, navigation, PP v1.0

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,27 @@
 const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.navigation');
 
+// No-js menu
+toggle.classList.remove('toggle--off');
+menu.classList.remove('menu--open');
+menuNav.classList.remove('navigation--open');
+menuNav.classList.remove('navigation--no-js');
+
+// Кнопка гамбургер
 if (toggle) {
   toggle.addEventListener('click', (evt) => {
-    (toggle.classList.contains('is-active') === true) ? toggle.classList.remove('is-active') : toggle.classList.add('is-active');
+    (toggle.classList.contains('is-active') === true) ? closeMenu() : openMenu();
   });
+}
+
+function openMenu() {
+  toggle.classList.add('is-active');
+  menuNav.classList.add('navigation--open');
+  menu.classList.add('menu--open');
+}
+function closeMenu() {
+  toggle.classList.remove('is-active');
+  menuNav.classList.remove('navigation--open');
+  menu.classList.remove('menu--open');
 }
