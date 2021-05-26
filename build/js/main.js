6b6e1b90c8dd710e20d9965d70a427b43a9ad121
Евгений <jenek95@mail.ru>
2021-05-27 02:48:17 +0300

    fix v3

--- build/js/main.js
+++ build/js/main.js
@@ -1,13 +1,16 @@
+
 const inputNumberPhone = document.querySelector('#user-tel');
 if (inputNumberPhone) {
   inputNumberPhone.addEventListener('keyup', (evt) => {
-    inputNumberPhone.value = inputNumberPhone.value.replace(/[^\d]/g, '');
+    inputNumberPhone.value = inputNumberPhone.value.replace(/[^\d^(^)^+^ ]/g, '');
   });
 }
 
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.navigation');
+const body = document.querySelector('.page__body');
+
 // // No-js menu
 if (menu) {
   menu.classList.remove('menu--open');
@@ -30,12 +33,15 @@ function openMenu() {
   toggle.classList.add('is-active');
   menuNav.classList.add('navigation--open');
   menu.classList.add('menu--open');
+  body.classList.add('overflow');
 }
 function closeMenu() {
   toggle.classList.remove('is-active');
   menuNav.classList.remove('navigation--open');
   menu.classList.remove('menu--open');
+  body.classList.remove('overflow');
 }
+
 
 // Smooth scroll
 const links = document.querySelectorAll('a[href^="#"]');
@@ -44,7 +50,7 @@ if (links) {
     link.addEventListener('click', function (evt) {
       evt.preventDefault();
       const id = link.getAttribute('href');
-
+      closeMenu();
       document.querySelector(id).scrollIntoView({
         behavior: 'smooth',
         block: 'start'
--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.navigation');
+const body = document.querySelector('.page__body');
+
 // // No-js menu
 if (menu) {
   menu.classList.remove('menu--open');
@@ -23,9 +25,12 @@ function openMenu() {
   toggle.classList.add('is-active');
   menuNav.classList.add('navigation--open');
   menu.classList.add('menu--open');
+  body.classList.add('overflow');
 }
 function closeMenu() {
   toggle.classList.remove('is-active');
   menuNav.classList.remove('navigation--open');
   menu.classList.remove('menu--open');
+  body.classList.remove('overflow');
 }
+
